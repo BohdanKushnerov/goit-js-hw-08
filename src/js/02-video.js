9a60fbd7a67e58b4efc737874124c18d53d156e8
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-01-29 16:17:31 +0200

    Add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function dataSeconds(data) {
+  localStorage.setItem("videoplayer-current-time", data.seconds )
+}
+
+    player.on('timeupdate', throttle(dataSeconds, 1000));
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+      // seconds = the actual time that the player seeked to
+  }).catch(function(error) {
+      switch (error.name) {
+          case 'RangeError':
+            console.log(error.name)
+              // the time was less than 0 or greater than the video’s duration
+              break;
+  
+          default:
+              // some other error occurred
+              break;
+      }
+  });
