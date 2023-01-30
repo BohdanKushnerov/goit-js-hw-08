8b303ca485e7cb77b89a8bf8eb73ed64e34ba272
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-01-30 12:25:37 +0200

    Add changes to task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,7 +7,7 @@ const textareaEl = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = "feedback-form-state";
 const formData = {};
-console.log(formData);
+// console.log(formData);
 
 formEl.addEventListener('submit', onFormSubmit);
 formEl.addEventListener('input', throttle(onFormSaveChangeText, 500));
@@ -15,18 +15,32 @@ formEl.addEventListener('input', throttle(onFormSaveChangeText, 500));
 getTextFromStorage();
 
 function onFormSubmit(e) {
+  // console.log({
+  //   Email: e.target.email.value, 
+  //   Message: e.target.message.value
+  // })
+  const {
+    elements: { email, message }
+  } = e.target;
+  console.log({ 
+    Email: email.value, 
+    Message: message.value 
+  });
+
   e.preventDefault();
-  console.log('Submit form');
+  // console.log('Submit form');
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  const formData = {};
+  // console.log(formData);
 };
 
 function onFormSaveChangeText(e) {
-  console.log(e.target.value)
+  // console.log(e.target.value);
   formData[e.target.name] = e.target.value;
 
   const myJSON = JSON.stringify(formData);
-  console.log(myJSON);
+  // console.log(myJSON);
 
   const saveLocalStorage = localStorage.setItem(STORAGE_KEY, myJSON)
 };
@@ -35,7 +49,7 @@ function getTextFromStorage() {
   const getLocalStorage = localStorage.getItem(STORAGE_KEY);
 
   const parseStorage = JSON.parse(getLocalStorage);
-  console.log(parseStorage);
+  // console.log(parseStorage);
 
   if(getLocalStorage) {
     formData.email = parseStorage.email;
